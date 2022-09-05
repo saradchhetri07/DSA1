7f6d75c6f06ccb427523911936f28772dee11177
sarad chhetri <61595122+saradchhetri07@users.noreply.github.com>
2022-09-05 20:48:07 +0545

    Create quickSort.cpp

---
+++ quickSort.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+int partition(int *arr,int s,int e){
+  int pivot = arr[s];
+  int count =0;
+  for(int i=s+1;i<=e;i++){
+    if(arr[i]<pivot){
+      count++;
+    }
+  }
+  int pivotIndex = s+count;
+swap(arr[s],arr[pivotIndex]);
+  int i=0;
+  int j=e;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  // int pivot = arr[s];
+  // int count=0;
+  // for(int i=s+1;i<=e;i++){
+  //   if(arr[i]<=pivot){
+  //     count++;
+  //   }
+  // }
+  // // pivot = arr[count];
+  // int pivotIndex = s+count;
+  // swap(arr[s],arr[pivotIndex]);
+  // int i=s;
+  // int j=e-1;
+  // // while(i<pivotIndex && j>pivotIndex){
+  // //   while(arr[i]<pivot){
+  // //     i++;
+  // //   }
+  // //   while(arr[j]>pivot){
+  // //     j--;
+  // //   }
+  // //   if(i<pivotIndex && j>pivotIndex){
+  // //     swap(arr[i++],arr[j--]);
+  // //   }
+    
+  // // }
+  while(i<=j){
+    if(arr[i]<pivot){
+      i++;
+    }
+    if(arr[j]>pivot){
+      j--;
+    }
+    if(arr[i]>pivot && arr[j]<pivot){
+      swap(arr[i++],arr[j--]);
+    }else{
+      i++;
+      j--;
+    }
+  }
+  return pivotIndex;
+}
+void quickSort(int *arr,int s,int e){
+  if(s>=e){
+    return;
+  }
+  int pivot=partition(arr,s,e);
+  
+   return quickSort(arr,s,pivot-1);
+   return quickSort(arr,pivot+1,e);
+  
+}
+int main() {
+  int arr[] = {50,40,20,10,5,100};
+  int n = end(arr)-begin(arr);
+  quickSort(arr,0,n-1);
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+  
+}
