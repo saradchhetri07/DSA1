b244162e9d4a2c90f7a13b10480d30237e3061c5
sarad chhetri <61595122+saradchhetri07@users.noreply.github.com>
2022-09-07 15:06:02 +0545

    Create recursion_subsets.cpp

---
+++ recursion_subsets.cpp
@@ -0,0 +1,24 @@
+void solve(vector<int> nums,int index,vector<vector<int>>& ans,vector<int> output){
+    if(index==nums.size()){
+        ans.push_back(output);
+        return;
+    }
+    //exclude
+    solve(nums,index+1,ans,output);
+ 
+    int element = nums[index];
+    output.push_back(element);
+    solve(nums,index+1,ans,output);
+}
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index=0;
+        solve(nums,index,ans,output);
+        return ans;
+        
+    }
+};
